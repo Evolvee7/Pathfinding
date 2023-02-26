386b281f4d59a425b903c4b5153004e47d43976b
unreformable1 <danieltroszczynski@wp.pl>
2023-02-26 14:35:40 +0100

    Add equal operator to Vec2i struct

--- include/Vec2i.hpp
+++ include/Vec2i.hpp
@@ -10,4 +10,9 @@ struct Vec2i
     Vec2i(int x, int y): x(x), y(y)
     {
     }
+
+    bool operator==(const Vec2i& other_pos) const
+    {
+        return x == other_pos.x && y == other_pos.y;
+    }
 };
